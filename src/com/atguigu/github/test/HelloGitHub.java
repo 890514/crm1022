74a1e9da089613ec99587822bacc30315b0eaef4
890514 <xifeng890514@163.com>
2016-03-18 23:09:05 +0800

    ok

--- src/com/atguigu/github/test/HelloGitHub.java
+++ src/com/atguigu/github/test/HelloGitHub.java
@@ -5,5 +5,6 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		System.out.println("hello GitHub");
 		System.out.println("hello GitHub,你好！");
+		System.out.println("local 收到,你好！");
 	}
 }
